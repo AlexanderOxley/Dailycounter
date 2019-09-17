031999258b6f55b9f98abfe7b1d841014ada702e
Alexander Oxley <alexanderoxley@gmail.com>
2019-09-17 11:22:42 +0200

    Initial Commit

---
+++ Dailycounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dailycounter
+//
+//  Created by Alexander Oxley on 17.09.19.
+//  Copyright © 2019 Alexander Oxley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
